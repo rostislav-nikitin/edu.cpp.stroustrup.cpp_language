cb97ad154482a8ddce6a65ed52f2e12a967b5d81
Rostyslav Nikitin <rostislav.nikitin@gmail.com>
2024-01-10 23:19:16 +0200

    Bitsets examples implemented

--- src/chap-017/absense/absense.cpp
+++ src/chap-017/absense/absense.cpp
@@ -6,8 +6,9 @@ int main()
 {
 	std::map<std::string, std::bitset<32>> visit_list;
 
-	visit_list["John Doe"] = std::bitset<32>(0b010101010101);
+	visit_list["John Doe"] = std::bitset<32>(0b010101010101011111);
 	visit_list["Peter Valda"] = std::bitset<32>(0b111111111111);
+	visit_list["Sobak"] = std::bitset<32>(0b000000000111);
 
 	for(std::map<std::string, std::bitset<32>>::iterator it = visit_list.begin(); it != visit_list.end(); ++it)
 	{
@@ -17,7 +18,9 @@ int main()
 				++acc;
 
 		if(acc == 12)
-			std::cout << it->first;
+			std::cout << it->first << "\t\t\tA" << std::endl;
+		else if(acc >= 8)
+			std::cout << it->first << "\t\t\t8" << std::endl;
 	}
 
 	return EXIT_SUCCESS;
